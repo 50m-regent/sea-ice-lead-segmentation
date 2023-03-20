9306019b9da94afefa265e52b975f2801a15e183
50m-regent <len.hirata@gmail.com>
2023-03-20 19:38:32 +0000

    hi

--- archive/archive/GOAD.py
+++
@@ -1,8 +0,0 @@
-
-
-if __name__ == '__main__':
-    learning_rate = 0.001
-    num_rots      = 32
-    batch_size    = 64
-    num_epochs    = 30
-    dim_out       = 4
--- archive/utils/normalize.py
+++
@@ -1,4 +0,0 @@
-import numpy
-
-def normalize(x):
-    return (x - numpy.min(x)) / (numpy.max(x) - numpy.min(x))
